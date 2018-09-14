fb0db4bb4314377c847d284b1f2e4fe1568c2a29
Gaurang Tandon <1gaurangtandon@gmail.com>
2018-09-14 21:55:08 +0530

    fixed overlap of navbar menu with banner and resolved transparency issues onscroll of banner

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,74 +1,80 @@
-var $win = $(window),
-    // native DOM-specific lookups are faster than jQuery selector lookups
-    $nav = $(document.getElementsByClassName("navbar")[0]),
-    $cov = $(document.getElementsByClassName("cover")[0]),
-    $jump = $(document.getElementById("jump")),
-    $jumpicon = $(document.getElementById("jumpicon")),
-    $banner = $(document.getElementById("banner")),
-    coverPageHeight = $cov.height(),
-    headingTopPosition = coverPageHeight / 2,
-    // make navbar opaque just before user scrolls past heading
-    navBarTransparentPixelLimit = headingTopPosition * 0.3,
-    transparentNavbarClass = "transparent";
+function init() {
+    var $win = $(window),
+        // native DOM-specific lookups are faster than jQuery selector lookups
+        $nav = $(document.getElementsByClassName("navbar")[0]),
+        $cov = $(document.getElementsByClassName("cover")[0]),
+        $jump = $(document.getElementById("jump")),
+        $jumpicon = $(document.getElementById("jumpicon")),
+        $banner = $(document.getElementById("banner")),
+        // some pages have it named .align-box, others h1
+        $heading = $(document.querySelector(".align-box, h1")),
+        coverPageHeight = $cov.height(),
+        headingTopPosition = $heading.offset().top,
+        // make navbar opaque just before user scrolls past heading
+        navBarTransparentPixelLimit =
+            headingTopPosition - $nav.height() - ($banner.height() || 0),
+        transparentNavbarClass = "transparent";
 
-function throttle(func, time) {
-    var timeout, hadCalledInBetween;
+    function throttle(func, time) {
+        var timeout, hadCalledInBetween;
 
-    return function() {
-        if (!timeout) {
-            func.apply(this, arguments);
+        return function() {
+            if (!timeout) {
+                func.apply(this, arguments);
 
-            timeout = setTimeout(function() {
-                if (hadCalledInBetween) {
-                    func.apply(this, arguments);
-                    hadCalledInBetween = false;
-                }
-                timeout = null;
-            }, time);
+                timeout = setTimeout(function() {
+                    if (hadCalledInBetween) {
+                        func.apply(this, arguments);
+                        hadCalledInBetween = false;
+                    }
+                    timeout = null;
+                }, time);
+            } else {
+                hadCalledInBetween = true;
+            }
+        };
+    }
+
+    function checkScroll() {
+        var hiddenClass = "hiddenJumpIcon",
+            scrollTop = $win.scrollTop();
+
+        if (scrollTop > navBarTransparentPixelLimit) {
+            $nav.removeClass(transparentNavbarClass);
+            $banner.removeClass("transparent-banner");
+            $banner.addClass("paint-banner");
         } else {
-            hadCalledInBetween = true;
+            $nav.addClass(transparentNavbarClass);
+            $banner.removeClass("paint-banner");
+            $banner.addClass("transparent-banner");
         }
-    };
-}
 
-function checkScroll() {
-    var hiddenClass = "hiddenJumpIcon",
-        scrollTop = $win.scrollTop();
+        if (scrollTop > coverPageHeight / 2) {
+            $jump.removeClass(hiddenClass);
+            $jumpicon.removeClass(hiddenClass);
+        } else {
+            $jump.addClass(hiddenClass);
+            $jumpicon.addClass(hiddenClass);
+        }
+    }
 
-    if (scrollTop > navBarTransparentPixelLimit) {
+    // demo for throttle https://jsbin.com/sagiwizuvu/1/edit?output
+    if ($cov.length > 0) {
+        checkScroll();
+        $win.on("scroll load resize", throttle(checkScroll, 100));
+    } else {
+        // make navbar permanently opaque on non-Home pages
         $nav.removeClass(transparentNavbarClass);
-        $banner.removeClass("transparent-banner");
-        $banner.addClass("paint-banner");
-    } else {
-        $nav.addClass(transparentNavbarClass);
-        $banner.removeClass("paint-banner");
-        $banner.addClass("transparent-banner");
     }
 
-    if (scrollTop > coverPageHeight) {
-        $jump.removeClass(hiddenClass);
-        $jumpicon.removeClass(hiddenClass);
-    } else {
-        $jump.addClass(hiddenClass);
-        $jumpicon.addClass(hiddenClass);
-    }
+    $jump.click(function() {
+        $("body, html").animate(
+            {
+                scrollTop: 0
+            },
+            500
+        );
+    });
 }
 
-// demo for throttle https://jsbin.com/sagiwizuvu/1/edit?output
-if ($cov.length > 0) {
-    checkScroll();
-    $win.on("scroll load resize", throttle(checkScroll, 100));
-} else {
-    // make navbar permanently opaque on non-Home pages
-    $nav.removeClass(transparentNavbarClass);
-}
-
-$jump.click(function() {
-    $("body, html").animate(
-        {
-            scrollTop: 0
-        },
-        500
-    );
-});
-
+window.addEventListener("load", init);
